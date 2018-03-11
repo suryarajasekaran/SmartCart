3fe10819d88bc34d288e0ed9260fcdb5bf2677f4
Surya Rajasekaran <surya.rajasekaran.26@gmail.com>
2018-03-10 21:34:38 -0800

    fix frontend

--- frontend/static/js/getList.js
+++ frontend/static/js/getList.js
@@ -1,42 +1,66 @@
-//JS to populate abbreviations/names of  all stations
+getList()
+setInterval(function() {
+    getList()
+}, 1000 * 60 * 1); // where X is your every X minutes
 
-$.ajax({
-    url: 'http://ec2-54-183-133-199.us-west-1.compute.amazonaws.com:8881/list',
-    type: "GET",
-    dataType: "json",
-    data: {
-    },
-    //contentType: "application/json; charset=utf-8",
-    xhrFields: {
-                   withCredentials: true
-                },
-    crossDomain: true,
-    cache: false,
-    success: function (result) {
-        var dataset = result.data;
-        var departureStation = document.getElementById('departureStation');
-        var arrivalStation = document.getElementById('arrivalStation');
-        for(var i = 0; i < dataset.length; i++) {
-            var opt = document.createElement('option');
-            opt.innerHTML = dataset[i].name;
-            opt.value = dataset[i].abbr;
-            arrivalStation.appendChild(opt);
-        };
-        for(var i = 0; i < dataset.length; i++) {
-            var opt = document.createElement('option');
-            opt.innerHTML = dataset[i].name;
-            opt.value = dataset[i].abbr;
-            departureStation.appendChild(opt);
-        };
-    },
-    error: function () {
-        console.log("get Station.js error");
-    }
-});
+function getList() {
+    $.ajax({
+        url: 'http://ec2-54-183-133-199.us-west-1.compute.amazonaws.com:8881/list?state=active',
+        type: "GET",
+        dataType: "json",
+        data: {
+        },
+        //contentType: "application/json; charset=utf-8",
+        xhrFields: {
+                       withCredentials: true
+                    },
+        crossDomain: true,
+        cache: false,
+        success: function (result) {
+            var dataset = result;
+            console.log(dataset);
+            var list = document.getElementById('list');
+            list.innerHTML = ""
+            for(var i = 0; i < dataset.length; i++) {
+                //dataset[i].sensor_id;
+                //dataset[i].product_id;
+                //dataset[i].timestamp;
+                //dataset[i].sensor_type;
+                //dataset[i].client_id;
+                var li = document.createElement('li');
+                li.className = ('row')
+                li.innerHTML = '<span class="popbtn"><a class="arrow"></a></span>'
 
-$(function(){
-    $.getJSON('', function(data) {
-        console.log(data);
+                var slNo = document.createElement('span');
+                slNo.className = ('quantity')
+                slNo.innerHTML = i+1;
+                li.appendChild(slNo)
 
+                var productId = document.createElement('span');
+                productId.className = ('itemName')
+                productId.innerHTML = dataset[i].product_id;
+                li.appendChild(productId)
+
+                var sensorType = document.createElement('span');
+                sensorType.className = ('price')
+                sensorType.innerHTML = dataset[i].sensor_type;
+                li.appendChild(sensorType)
+
+
+
+                list.appendChild(li)
+            };
+            /*
+            <li class="row">
+                <span class="quantity">1</span>
+                <span class="itemName">Birthday Cake</span>
+                <span class="popbtn"><a class="arrow"></a></span>
+                <span class="price">$49.95</span>
+            </li>
+            */
+        },
+        error: function () {
+            console.log("getList.js error");
+        }
     });
-});
+}
