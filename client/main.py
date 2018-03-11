57ec07cbd61a089cf5a3d72af12f60bd6fe7eeb6
root <surya.rajasekaran.26@gmail.com>
2018-03-11 11:56:31 +0000

    raspberry

--- client/main.py
+++ client/main.py
@@ -1,3 +1,9 @@
+import RPi.GPIO as GPIO
+>>> GPIO.setmode(GPIO.BCM)
+>>> GPIO.setup(23,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+>>> print GPIO.input(23)
+0
+>>> print GPIO.input(23)
 import schedule
 import time
 
