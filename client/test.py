38cddd80092f9b023547500ea87b5ecbde791695
Surya Rajasekaran <surya.rajasekaran.26@gmail.com>
2018-03-16 13:08:00 -0700

    fix

--- client/test.py
+++ client/test.py
@@ -6,8 +6,8 @@ from client import *
 
 def get_button_value():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    return GPIO.input(16)
+    GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    return GPIO.input(18)
 
 def manual_button_check():
     print "button value is "+str(get_button_value())
